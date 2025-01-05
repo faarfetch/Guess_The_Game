ec3abf6e3eacc7c0db454ae18a70b4ae6d3a19bb
Giuliaf28 <forlanigiulia06@gmail.com>
2025-01-05 21:05:22 +0100

    modifica a login e registrazione

--- gestori/gestoreUtenti.php
+++ gestori/gestoreUtenti.php
@@ -1,14 +1,16 @@
-<?php  
+<?php
 //gestore degli utenti
 
 class gestoreUtenti
 {
 
-    public function __construct() {}
+    public function __construct()
+    {
+    }
 
     public function login($nome, $password)
     {
-        $filePath = "../files/users/" . $nome . ".json";
+        /*$filePath = "../files/users/users.csv";
         if (file_exists($filePath)) {
             $userData = json_decode(file_get_contents($filePath), true);
 
@@ -16,23 +18,24 @@ class gestoreUtenti
                 return 1;
             }
         }
+        return 0;*/
+
+        $filePath = file_get_contents("../files/users/users.csv");
+        $utenti=explode("\r\n", $filePath);
+        foreach ($utenti as $utente) {
+            $campi=explode(";", $utente);
+            if($campi[1]==$password) // username;pwd;punteggio
+                return 1;
+        }
+
         return 0;
     }
 
     public function registrazione($nome, $password)
     {
-        $filePath = "../files/users/" . $nome . ".json";
-        if (file_exists($filePath)) {
-            return 0;
-        }
-
-        $userData = array(
-            'username' => $nome,
-            'password' => $password,
-            'punteggio' => 0
-        );
-
-        file_put_contents($filePath, json_encode($userData));
+        $filePath = "../files/users/users.csv";
+        $userData = $nome . ";" . $password . ";" . "0";
+        file_put_contents($filePath, $userData);
         return 1;
     }
 }
